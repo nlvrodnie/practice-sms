1436c6a1eb5f1c4ac683021bd42497b6c4152371
Rodnie Lecera <rlecera@netlinkvoice.com>
2022-06-24 16:08:20 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'firstname' => 'Admin',
+        //     'lastname' => 'Admin',
+        //     'email' => 'admin@example.com',
+        // ]);
+        $this->call(RolesAndPermissionSeeder::class);
+        $this->call(UserSeeder::class);
+    }
+}
